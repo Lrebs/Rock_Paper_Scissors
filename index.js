fe5fbd04dab7eb6567d5865c60cfa44cbb5e5a4f
lucas R <toros_.88@hotmail.com>
2022-06-22 23:19:49 -0400

    function playRound() started but not finished

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ function computerPlay() {
   const computerChoice = choices[randomChoice];
   return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    return "Draw!";
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    return "Draw!";
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return "Draw!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "you win! rock beats scissors!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "you lose! paper beats rock!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "you lose! paper beats rock!";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "you lose! scissors beats paper!";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "you lose! rock beats scissors!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "you win! rock scissors beat paper!";
+  }
+
+  console.log(playerSelection, computerSelection);
+}
+playRound();
