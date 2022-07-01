52707a063a8d4efe476063a78cd13b7342a9f7f9
lucas R <toros_.88@hotmail.com>
2022-06-30 23:54:06 -0400

    cleaned up function computerPlay and function playRound

---
+++ app.js
@@ -0,0 +1,35 @@
+function computerPlay() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomChoice = Math.floor(Math.random() * choices.length);
+  const computerChoice = choices[randomChoice];
+  return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    return "Draw!";
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    return "Draw!";
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return "Draw!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "you win! rock beats scissors!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "you lose! paper beats rock!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "you win! paper beats rock!";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "you lose! scissors beats paper!";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "you lose! rock beats scissors!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "you win! scissors beats paper!";
+  }
+}
+const playerSelection = "paper";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
